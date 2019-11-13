73ae17abb21fc33d0670614c2b30b2d28498b49d
Magnus V Jensen <magnusvjensen@gmail.com>
2019-11-13 21:46:57 +0100

    First Commit: Gatsby, Sass, Styled-component, Contentful env setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import styled from "styled-components"
+import { colors } from "../styles/global-js/colors"
+import "../styles/global-css/nomalize/normalize.scss"
+
+const StyleCompTest = styled.h1`
+  color: ${colors.green};
+  font-size: 2rem;
+`
+
+const indexPage = () => {
+  return (
+    <div>
+      <StyleCompTest>Hello World!</StyleCompTest>
+    </div>
+  )
+}
+
+export default indexPage
