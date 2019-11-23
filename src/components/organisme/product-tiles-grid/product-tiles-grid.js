37a704d08e4d99796de7818682cf6120bc21f360
Jakob <jako2889@stud.kea.dk>
2019-11-23 19:12:28 +0100

    added React-id-swiper & added to grid (needs to be responsive)

--- src/components/organisme/product-tiles-grid/product-tiles-grid.js
+++ src/components/organisme/product-tiles-grid/product-tiles-grid.js
@@ -2,6 +2,8 @@ import React from "react"
 import styled from "styled-components"
 
 import ProductTile from "../../molecules/product-tile/product-tile"
+import Swiper from 'react-id-swiper'
+import "swiper/css/swiper.css"
 
 const Wrapper = styled.div`
   width: 100%;
@@ -10,14 +12,29 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
 `
+const SwiperGrid = styled(Swiper)`
+
+`
+const ProductGrid = styled.div`
+`
 
 const ProductTilesGrid = props => {
+
+    const params = {
+        slidesPerView: 3,
+        spaceBetween: 5
+      }
+
   return (
     <Wrapper>
       <Container>
+      <SwiperGrid {...params}>
           {props.products.map( product => (
+            <ProductGrid>
               <ProductTile title={product.title} price={product.price} img={product.img} />
+            </ProductGrid>
           ))}
+      </SwiperGrid>
       </Container>
     </Wrapper>
   )
--- src/components/organisme/product-tiles-grid/product-tiles-grid.stories.js
+++ src/components/organisme/product-tiles-grid/product-tiles-grid.stories.js
@@ -27,6 +27,26 @@ const products = [
         price: "1.099,00 kr",
         img: "https://dummyimage.com/600x600/000/fff.jpg"
     },
+    {
+        title: "Fedora Furfelt - Stetson",
+        price: "1.099,00 kr",
+        img: "https://dummyimage.com/600x600/000/fff.jpg"
+    },
+    {
+        title: "Fedora Furfelt - Stetson",
+        price: "1.099,00 kr",
+        img: "https://dummyimage.com/600x600/000/fff.jpg"
+    },
+    {
+        title: "Fedora Furfelt - Stetson",
+        price: "1.099,00 kr",
+        img: "https://dummyimage.com/600x600/000/fff.jpg"
+    },
+    {
+        title: "Fedora Furfelt - Stetson",
+        price: "1.099,00 kr",
+        img: "https://dummyimage.com/600x600/000/fff.jpg"
+    },
 ]
 
 export const productTile = () => <ProductTilesGrid products={products} />
