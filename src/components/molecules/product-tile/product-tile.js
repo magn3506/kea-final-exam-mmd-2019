0c0cccd9c4a5ded990d6c73f91cbf0bed99fa3c8
Jakob <jako2889@stud.kea.dk>
2019-11-23 12:49:05 +0100

    Added breakpoints

--- src/components/molecules/product-tile/product-tile.js
+++ src/components/molecules/product-tile/product-tile.js
@@ -1,9 +1,18 @@
 import React from "react"
 import styled from "styled-components"
 import { colors } from "../../../styles/global-js/colors"
+import { device } from "../../../styles/global-js/breakpoints"
 
 const Wrapper = styled.div`
-  width: 225px;
+  width: 100px;
+
+  @media ${device.tablet} {
+    width: 175px;
+  }
+
+  @media ${device.laptop} {
+    width: 225px;
+  }
 `
 
 const InfoBox = styled.div`
@@ -13,11 +22,31 @@ const ImgBox = styled.img`
 width: 100%;
 `
 
-const ProductTitle = styled.h4``
+const ProductTitle = styled.h4`
+font-size: 0.8rem;
+margin-top: 0;
+
+  @media ${device.tablet} {
+    font-size: 1rem;
+  }
+
+  @media ${device.laptop} {
+    font-size: 1rem;
+  }
+`
 
 const ProductPrice = styled.p`
   color: ${colors.gold};
   font-weight: 600;
+  font-size: 0.8rem;
+
+  @media ${device.tablet} {
+    font-size: 1rem;
+  }
+
+  @media ${device.laptop} {
+    font-size: 1rem;
+  }
 `
 
 const ProductTile = props => {
