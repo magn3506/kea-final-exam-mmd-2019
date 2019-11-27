22c306f0503507c5ffb3b851d2fccd7a5225751a
Magnus V Jensen <magnusvjensen@gmail.com>
2019-11-27 17:45:43 +0100

    update for filter prop

--- src/pages/products.js
+++ src/pages/products.js
@@ -18,7 +18,7 @@ class ProductsPage extends Component {
       <Layout siteType={true}>
         <MainContentTestRemoveMe>
           <h1>Products Page</h1>
-          {this.props.location.state.filter ? (
+          {this.props.location.state.filter || undefined ? (
             <p>{this.props.location.state.filter}</p>
           ) : (
             <p>"No props"</p>
