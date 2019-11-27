94ba4dd2dd3484ddc4173872f8ceb828d0da5d2d
Jakob <jako2889@stud.kea.dk>
2019-11-27 23:14:41 +0100

    added title cta & splash image

---
+++ src/components/atoms/TitleCTA/TitleCTA.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import TitleCTA from "./TitleCTA"
+
+export default {
+  component: TitleCTA,
+  title: "Components|Atoms/TitleCTA",
+}
+
+export const titleCTA = () => <TitleCTA title="The essential collection" cta="Shop Now"/>
--- src/pages/butique.js
+++ src/pages/butique.js
@@ -2,28 +2,30 @@ import Layout from "../components/layout/layout"
 import styled from "styled-components"
 
 import React, { Component } from "react"
+import SplashImage from "../components/atoms/splashImage/splashImage"
+import TitleCTA from "../components/atoms/TitleCTA/TitleCTA"
+
+const img = "https://dummyimage.com/1200x1000/000/fff.jpg"
 
 export class ButiqueMainPage extends Component {
   state = {
     // True = butique, False = bar
     siteType: true,
   }
+
   render() {
-    const MainContentTestRemoveMe = styled.div`
+    const MainContent = styled.div`
       max-width: 1000px;
       margin: 0 auto;
-      background: grey;
       height: 200vh;
-      text-align: center;
-      font-size: 2em;
-      padding-top: 50px;
     `
 
     return (
       <Layout siteType={this.state.siteType}>
-        <MainContentTestRemoveMe>
-          <h1>Butique Main Page</h1>
-        </MainContentTestRemoveMe>
+        <MainContent>
+            <SplashImage img={img}/>
+            <TitleCTA title="The essential collection" cta="Shop Now"/>
+        </MainContent>
       </Layout>
     )
   }
