d65dd853de4dd86caea59ccaf34e7a1c5e06d92b
Jakob <jako2889@stud.kea.dk>
2019-11-28 22:28:11 +0100

    Added graphql query for product tiles

--- src/pages/butique.js
+++ src/pages/butique.js
@@ -1,7 +1,7 @@
 import Layout from "../components/layout/layout"
 import styled from "styled-components"
 
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { device } from "../styles/global-js/breakpoints"
 
 import SplashImage from "../components/atoms/splashImage/splashImage"
@@ -10,6 +10,7 @@ import ImageBanner from "../components/molecules/imageBanner/imageBanner"
 import ProductTilesGrid from "../components/organisme/product-tiles-grid/product-tiles-grid"
 import CategoryTilesGrid from "../components/molecules/category-tiles-grid/category-tiles-grid"
 import ProductTilesSection from "../components/organisme/product-tiles-section/product-tiles-section"
+import { StaticQuery, graphql } from "gatsby"
 
 /* DUMMY DATA SKAL SLETTES OG ERSTATTES MED CONTENTFUL DATA */
 const img = "https://dummyimage.com/1200x1000/000.jpg"
@@ -65,6 +66,13 @@ const products = [
   },
 ]
 
+/* DUMMY DATA SKAL SLETTES OG ERSTATTES MED CONTENTFUL DATA */
+
+const MainContent = styled.div`
+max-width: 1000px;
+margin: 0 auto;
+`
+
 const SaleSection = styled.div`
 background: #292929;
 padding-bottom: 25px;
@@ -96,38 +104,62 @@ padding-top: 25px;
   }
 `
 
-/* DUMMY DATA SKAL SLETTES OG ERSTATTES MED CONTENTFUL DATA */
+export default props => (
+  <StaticQuery 
+  query={
+    graphql`
+    query {
+      allContentfulProducts(sort: { fields: title, order: DESC }) {
+        edges {
+          node {
+            title
+            price
+            img {
+              resize(width: 300, height: 300) {
+                src
+              }
+            }
+            id
+            slug
+          }
+        }
+      }
+    }
+    `}
+    render={data => <ButiqueMainPage data={data} {...props}  />}
+    />
+)
 
-export class ButiqueMainPage extends Component {
-  state = {
-    // True = butique, False = bar
-    siteType: true,
-  }
+const ButiqueMainPage = ({ data }) => {
+  // state = {
+  //   // True = butique, False = bar
+  //   siteType: true,
+  // }
+  const products2 = data.allContentfulProducts.edges;
+  console.log(products2);
 
-  render() {
-    const MainContent = styled.div`
-      max-width: 1000px;
-      margin: 0 auto;
-    `
+  const test = products2.map( e => {
+    return e.node.img[0].resize.src
+  })
+console.log(test);
+  const [siteType, setSiteType] = useState(true);
 
     return (
-      <Layout siteType={this.state.siteType}>
+      <Layout siteType={siteType}>
         <MainContent>
           <SplashImage img={img} />
           <TitleCTA title="The essential collection" cta="Shop Now" />
-          <ProductTilesGrid products={products}/>
+          <ProductTilesGrid products={products2}/>
           <ImageBanner img={img} text="Some Promotion image and title for bar &amp; café fx. Social shopping drink menu" />
           <CategoryTilesGrid img={categoryImg} title="SHOP BY CATEGORY"/>
           <SaleSection>
-          <ProductTilesSection products={products} title="Sale Sale Sale" cta="See all" />
+          <ProductTilesSection products={products2} title="Sale Sale Sale" cta="See all" />
           </SaleSection>
           <GiftSection>
-          <ProductTilesSection products={products} title="Great grifts for any occassion" cta="See all" />
+          <ProductTilesSection products={products2} title="Great grifts for any occassion" cta="See all" />
           </GiftSection>
         </MainContent>
       </Layout>
     )
   }
-}
 
-export default ButiqueMainPage
