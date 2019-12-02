c19b47ae4b53c14b3e4deda26ef1c8bec8608c79
Magnus V Jensen <magnusvjensen@gmail.com>
2019-12-02 17:46:22 +0100

    index page start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,53 @@
-import styled from "styled-components"
+import React from "react"
 import { colors } from "../styles/global-js/colors"
-import Layout from "../components/layout/layout"
+import { device } from "../styles/global-js/breakpoints"
+import { GlobalStyle } from "../styles/global-js/globalstyles"
+import HBBSymbolText from "../components/atoms/hbb-symbol-title/hbb-symbol-title"
+import { Link } from "gatsby"
 
-import React, { Component } from "react"
+import styled from "styled-components"
 
-export class IndexPage extends Component {
-  state = {
-    // True = butique, False = bar
-    siteType: true,
-  }
+const IndexPage = () => {
+  const Wrapper = styled.div`
+    background: black;
+    width: 100vw;
+    height: 100vh;
+    padding: 50px;
+  `
+  const Container = styled.div`
+    max-width: 350px;
+    margin: 0 auto;
+  `
 
-  render() {
-    const MainContentTestRemoveMe = styled.div`
-      max-width: 1000px;
-      margin: 0 auto;
-      background: grey;
-      height: 200vh;
-      text-align: center;
-      font-size: 2em;
-      padding-top: 50px;
-    `
+  const TitleSVGContainer = styled.div`
+    width: 100%;
+  `
+  const Text = styled.div`
+    color: ${colors.gold};
+    font-size: 1.5rem;
+    text-align: center;
+    font-family: "Roboto Slab", serif;
+    width: 100%;
+    margin: 50px 0px;
+  `
+  const LinkContainer = styled.div``
+  const LinkBoutique = styled(Link)``
+  const LinkBar = styled(Link)``
 
-    return (
-      <Layout siteType={this.state.siteType}>
-        <MainContentTestRemoveMe>INDEX PAGE</MainContentTestRemoveMe>
-      </Layout>
-    )
-  }
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Wrapper>
+        <Container>
+          <TitleSVGContainer>
+            <HBBSymbolText></HBBSymbolText>
+          </TitleSVGContainer>
+          <Text>Choose which place you want to experience</Text>
+          <LinkContainer></LinkContainer>
+        </Container>
+      </Wrapper>
+    </>
+  )
 }
 
 export default IndexPage
