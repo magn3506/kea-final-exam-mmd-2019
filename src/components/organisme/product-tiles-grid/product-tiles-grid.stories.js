912309d0d97c3e090432edb656821882e67d2cfe
Magnus V Jensen <magnusvjensen@gmail.com>
2019-12-12 22:51:07 +0100

    fixes for dummt data storybook storires, price

--- src/components/organisme/product-tiles-grid/product-tiles-grid.stories.js
+++ src/components/organisme/product-tiles-grid/product-tiles-grid.stories.js
@@ -11,7 +11,7 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
         img: [
           {
             resize: {
@@ -27,7 +27,7 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
         img: [
           {
             resize: {
@@ -43,7 +43,7 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
         img: [
           {
             resize: {
@@ -59,7 +59,7 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
         img: [
           {
             resize: {
@@ -75,7 +75,7 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
         img: [
           {
             resize: {
@@ -91,7 +91,55 @@ const products = {
     {
       node: {
         title: "Winters Fedora Hat, Almond - Baileys",
-        pricing: 149900,
+        price: "1.299,00",
+        img: [
+          {
+            resize: {
+              src:
+                "//images.ctfassets.net/yyhkqa4jdoli/59DY9tB1bS1wsP0GV0T47g/70e80032d4319dc1f51dd8ede921aa71/size-s-.jpg?w=300&h=300&fl=progressive&q=50&fit=fill",
+            },
+          },
+        ],
+        id: "85092e4b-60e2-57f5-be8d-79eadb2f14b82",
+        slug: "winters-fedora-hat",
+      },
+    },
+    {
+      node: {
+        title: "Winters Fedora Hat, Almond - Baileys",
+        price: "1.299,00",
+        img: [
+          {
+            resize: {
+              src:
+                "//images.ctfassets.net/yyhkqa4jdoli/59DY9tB1bS1wsP0GV0T47g/70e80032d4319dc1f51dd8ede921aa71/size-s-.jpg?w=300&h=300&fl=progressive&q=50&fit=fill",
+            },
+          },
+        ],
+        id: "85092e4b-60e2-57f5-be8d-79eadb2f14b82",
+        slug: "winters-fedora-hat",
+      },
+    },
+    {
+      node: {
+        title: "Winters Fedora Hat, Almond - Baileys",
+        price: "1.299,00",
+        img: [
+          {
+            resize: {
+              src:
+                "//images.ctfassets.net/yyhkqa4jdoli/59DY9tB1bS1wsP0GV0T47g/70e80032d4319dc1f51dd8ede921aa71/size-s-.jpg?w=300&h=300&fl=progressive&q=50&fit=fill",
+            },
+          },
+        ],
+        id: "85092e4b-60e2-57f5-be8d-79eadb2f14b82",
+        slug: "winters-fedora-hat",
+      },
+    },
+    {
+      node: {
+        title: "Winters Fedora Hat, Almond - Baileys",
+        price: "1.299,00",
         img: [
           {
             resize: {
